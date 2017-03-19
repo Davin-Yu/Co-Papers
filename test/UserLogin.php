eb8842976c3b5b92d184ab391db6454d0865de87
Davin Yu <difeng.yu@outlook.com>
2017-03-19 10:48:40 +0800

    tested Session

--- test/UserLogin.php
+++ test/UserLogin.php
@@ -2,8 +2,13 @@
 /**
 * This will authenticate the user login information from database
 * @param _username, _password
-* @return print 'Right!'' if success
+* @return print 'Right!...'' if success
 */
+  session_start();  //This help to keep track of user info. from page to page
+  if (!empty($_SESSION['user_id'])) {
+    echo "You have alreadly login";
+    exit;
+  }
   $Uname = $_GET["_username"];
   $Upass = $_GET["_password"];
   $connection = mysql_connect("localhost","root","");
@@ -15,10 +20,15 @@
   while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
     if ($row[1] == $Uname and $row[2] == $Upass) {
       $flag = true;
+      $db_user_id = $row[0];
+      $db_username = $row[1];
     }
   }
   if ($flag == true) {
+    $_SESSION['user_id'] = $db_user_id;
+    $_SESSION['username'] = $db_username;
     echo "Right!";
+    echo "You have login as ".$_SESSION['username'];
   }else {
     echo "Wrong!";
   }
