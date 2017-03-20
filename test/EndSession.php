ff0d94ccb04932bf4449b4f82ee50784fcdd94ba
Davin Yu <difeng.yu@outlook.com>
2017-03-20 21:20:08 +0800

    add log out

--- test/EndSession.php
+++ test/EndSession.php
@@ -3,6 +3,9 @@
 * This will end the using Session.
 * Typiclly can be used in log out.
 */
+  session_start();
+  echo 'You Log our as'.$_SESSION['username'];
+
   session_destroy();
   unset($_SESSION['user_id']);
   unset($_SESSION['username']);
