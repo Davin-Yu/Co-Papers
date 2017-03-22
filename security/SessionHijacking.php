6af62bde3042a3bbc29b6682d19fd397d41b339e
Davin Yu <difeng.yu@outlook.com>
2017-03-22 16:08:35 +0800

    add md5 and other security issues

---
+++ security/SessionHijacking.php
@@ -0,0 +1,16 @@
+<? php
+/**
+* This record user IP address prevent hijacking happened
+*/
+  session_start();
+  $user_check = md5($_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR']);
+  if (empty($_SESSION['username'])) {
+    session_regenerate_id();
+    $_SESSION['user_data'] = $user_check;
+  }
+  if (strcmp($_SESSION['user_data'],$user_check) !== 0) {
+    session_regenerate_id();
+    $_SESSION = array();
+    $_SESSION['user_data'] = $user_check;
+  }
+ ?>
