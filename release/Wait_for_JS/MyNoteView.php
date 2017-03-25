a371b09cd3efb8467ce3f82162018b370f789774
Davin Yu <difeng.yu@outlook.com>
2017-03-25 17:19:17 +0800

    add MyNoteView and OtherNoteView to release

---
+++ release/Wait_for_JS/MyNoteView.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+			<title>MyNoteView</title>
+			<link href="Style1.css" type=text/css rel=stylesheet>
+	</head>
+	<body>
+	<?php
+		require('getMyNoteView.php');
+	 ?>
+		<div class="user">
+			Hello <?php echo $_SESSION["user_id"]?>  | Home Page | Quit
+		</div>
+		<div class="main">
+			<div class="part1">
+				<h1>
+					Co-Papers --- My Notes
+				</h1>
+			</div>
+			<form>
+				<div class="part2">
+					<h2><?php echo $_article ?></h2>
+					<div class="content">
+						<p> <?php echo $_note_content ?>
+						</p>
+					</div>
+					<div class="URL">
+					<p>Link: <a href="<?php echo $_article_url?>"> <?php echo $_article_url ?></a></p>
+					</div>
+					<hr/>
+					<?php
+							require('getComment.php')
+					 ?>
+				</div>
+				<div class="part3">
+					<input type="submit"  name="CreateANew" id="button1" value="Create a New">
+					<input type="button"  name="Edit" id="button1" value="Edit">
+				</div>
+		 </form>
+		</div>
+	</body>
+
+</html>
