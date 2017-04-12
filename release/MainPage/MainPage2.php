d92a8ccd8c56f26f8dc6eebe06e6d550a9504061
Davin Difeng Yu <difeng.yu@outlook.com>
2017-04-12 19:56:34 +0800

    new release

---
+++ release/MainPage/MainPage2.php
@@ -0,0 +1,10 @@
+<?php session_start(); ?>
+<html>
+  <body>
+    <div>
+      <a href="../UserLogin/UserLogin.html "> Log In </a>  &nbsp
+      <a href="../UserSignUp/UserSignUp.php "> Sign Up </a>  &nbsp
+    </div>
+    <img src="MainPage.png" alt="logo">
+  </body>
+</html>
