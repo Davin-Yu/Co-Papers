da9a9d11019ab063b9b32e60c957f9f3fb2ec4fb
Davin Difeng Yu <difeng.yu@outlook.com>
2017-05-03 21:18:00 +0800

    add premium

--- release/Premium/ifCanGoPremium.php
+++ release/Premium/ifCanGoPremium.php
@@ -1,3 +1,20 @@
 <?php
-  echo  "<script type='text/javascript'>alert('Wrong Code!');location.href='../Login_Main/Login_Main.php'</script>";
+  session_start();
+  $user_id = $_SESSION["user_id"];
+  $_code = $_POST["_code"];
+  require("../connectDB.php");
+  $result=mysql_query("SELECT * FROM InvitationCode");
+  if ($row=mysql_fetch_array($result)) {
+    if ($row[0] === $_code) {
+      $query = "INSERT INTO Premium VALUES ('','$user_id')";
+      $result2 = mysql_query($query);
+      if ($result2) {
+        echo  "<script type='text/javascript'>alert('Success!');location.href='../Login_Main/Login_Main.php'</script>";
+      } else {
+        echo "wrong";
+      }
+      exit();
+    }
+  }
+  //echo  "<script type='text/javascript'>alert('Wrong Code!');location.href='../Login_Main/Login_Main.php'</script>";
  ?>
