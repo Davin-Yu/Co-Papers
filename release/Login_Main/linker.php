8de8f9123d97226cfb61b83ee2ce7b7ba6fac1b1
Davin Difeng Yu <difeng.yu@outlook.com>
2017-05-08 08:58:10 +0800

    New Ajax for search result

--- release/Login_Main/linker.php
+++ release/Login_Main/linker.php
@@ -2,7 +2,7 @@
     $topic = $_POST["_topic"];
     $keyword = $_POST["_keyword"];
     if (empty($_POST["_keyword"])) {
-      echo  "<script> location.href='../RandomResult/RandomResult.php?_topic=$topic&_keyword=$keyword' </script>";
+      echo  "<script> location.href='../SearchedResult/SearchedResult.php?_topic=$topic";
     }
     echo  "<script> location.href='../SearchedResult/SearchedResult.php?_topic=$topic&_keyword=$keyword' </script>";
  ?>
--- release/RandomResult/linker.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $topic = $_POST["_topic"];
-    $keyword = $_POST["_keyword"];
-    if (empty($_POST["_keyword"])) {
-      echo  "<script> location.href='../RandomResult/RandomResult.php?_topic=$topic&_keyword=$keyword' </script>";
-    }
-    echo  "<script> location.href='../SearchedResult/SearchedResult.php?_topic=$topic&_keyword=$keyword' </script>";
- ?>
